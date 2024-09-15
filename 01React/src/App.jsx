0318a4f01e744ae76f82432fc57159944ff355b2
Himanshu Verma <himanshuv309@gmail.com>
2024-09-16 01:23:10 +0530

    First React Projects

---
+++ 01React/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <div class="relative h-[400px] w-[300px] rounded-md md-5">
+  <img
+    src="src/assets/Charlie.jpg"
+    alt="AirMax Pro"
+    class="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div class="absolute bottom-4 left-4 text-left">
+    <h1 class="text-lg font-semibold text-white">Delba</h1>
+    <p class="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      View Profile →
+    </button>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
