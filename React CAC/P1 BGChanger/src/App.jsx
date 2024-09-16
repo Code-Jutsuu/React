cae7033e437bee0b72d120c86150b4ae2d36050a
Himanshu Verma <himanshuv309@gmail.com>
2024-09-16 18:12:04 +0530

    basic project

---
+++ React CAC/P1 BGChanger/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <>
+      <div className="bg-black w-full h-screen">
+        <div>test</div>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
