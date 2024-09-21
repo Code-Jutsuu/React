701381cbbb588bf27e31e0578d68fd5d009b65fb
Himanshu Verma <himanshuv309@gmail.com>
2024-09-21 23:51:04 +0530

    react components

---
+++ React BC/my-react-app/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer(){
+
+    return(
+        <>
+            <p>&copy; {new Date().getFullYear()} Gaanja & Heroin Limited</p>
+
+        </>
+     );
+}
+
+export default Footer
---
+++ React BC/my-react-app/src/Header.jsx
@@ -0,0 +1,22 @@
+
+
+function Header(){
+
+    return(
+        <>
+            <h1> Web Component </h1>
+
+            <ul>
+                <li><a href="">Home</a> </li>
+                <li><a href="">About</a> </li>
+                <li><a href="">Services</a> </li>
+                <li><a href="">Contact</a> </li>
+            </ul>
+            <hr/>
+        </>
+        
+    );
+
+}
+
+export default Header 
