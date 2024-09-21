52905752cfc77b1a7f778ff22901201ea3ddae83
Himanshu Verma <himanshuv309@gmail.com>
2024-09-21 23:56:06 +0530

    components

---
+++ React BC/my-react-app/src/Food.jsx
@@ -0,0 +1,18 @@
+
+
+function Food(){
+
+    return(
+        <>
+            <ul>
+                <li> apple</li>
+                <li> weed</li>
+                <li> bhang</li>
+                <li> tarang</li>
+            </ul>
+            <hr />
+        </>
+    );
+}
+
+export default Food
