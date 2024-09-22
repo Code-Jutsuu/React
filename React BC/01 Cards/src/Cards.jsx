aa9523ce4630e1429247cd44af1ca661df7a0d7f
Himanshu Verma <himanshuv309@gmail.com>
2024-09-22 14:45:38 +0530

    card components

--- React BC/01 Cards/src/Cards.jsx
+++ React BC/01 Cards/src/Cards.jsx
@@ -6,7 +6,7 @@ function Cards(){
     return(
 
        
-         <div className="card border border-gray-400 rounded-lg shadow-md p-5 m-2 text-center max-w-xs inline-block justify-center items-center">
+         <div className="card border bg-cyan-800 border-gray-400 rounded-lg shadow-lg shadow-orange-300 p-5 m-2 text-center max-w-xs inline-block justify-center items-center">
             <img className='max-w-[60%] h-auto rounded-xl mb-3 ml-16 ' src="https://via.placeholder.com/150" alt="profile pic" />
             <h2 className="font-sans m-0 text-gray-800" > Baboon</h2>
             <p className="font-sans text-gray-700"> Baboon is like Gorilla and loves eating Banana</p>
