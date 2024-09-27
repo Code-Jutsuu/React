9cecc3e0c52b3a7645c1199b33207ce21749fa85
Himanshu Verma <himanshuv309@gmail.com>
2024-09-27 22:23:17 +0530

    counter program

---
+++ React BC/03 Counter Program/src/App.jsx
@@ -0,0 +1,21 @@
+// React hook = Special function that allows functional components
+//                         to use React features without writing class components 
+//                         (useState, useEffect, useContext, useReducer, and more...)                
+
+// useState() = A React hook that allows the creation of a stateful variable
+//                       AND a setter function to update its value in the Virtual DOM.
+//                       [name, setName]
+
+import
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
