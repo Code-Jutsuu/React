e3633831b09b107e1cfa2641cdf838e83f5bdd69
Himanshu Verma <himanshuv309@gmail.com>
2024-09-30 00:49:22 +0530

    hero section

--- Personal Portfolio/Portfolio/src/App.jsx
+++ Personal Portfolio/Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar.jsx"
-
+import Hero from "./components/Hero.jsx"
 
 function App() {
   
@@ -12,6 +12,7 @@ function App() {
     <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
     <div className="container mx-auto px-8">
       <Navbar/>
+      <Hero/>
     </div>
     
    </div>
