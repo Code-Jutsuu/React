a9be17eb4ef40ff6a32b5bdbc196d76e3a8cb521
Himanshu Verma <himanshuv309@gmail.com>
2024-10-01 00:01:57 +0530

    counter program

--- React BC/03 Counter Program/src/App.jsx
+++ React BC/03 Counter Program/src/App.jsx
@@ -6,16 +6,29 @@
 //                       AND a setter function to update its value in the Virtual DOM.
 //                       [name, setName]
 
-import
+import React, {useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Counter(){
 
-  return (
-    <>
-     
-    </>
-  )
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
+    return( <div className="counter-container text-center font-sans bg-slate-800 h-full">
+                <p className="count-display text-[10em] mt-0 mb-12">{count}</p>
+                <button className="counter-button w-[150px] h-[50px] text-2xl font-bold mx-1 bg-cyan-500 text-white rounded hover:bg-cyan-400" onClick={decrement}>Decrement</button>
+                <button className="counter-button w-[150px] h-[50px] text-2xl font-bold mx-1 bg-cyan-500 text-white rounded hover:bg-cyan-400" onClick={reset}>Reset</button>
+                <button className="counter-button w-[150px] h-[50px] text-2xl font-bold mx-1 bg-cyan-500 text-white rounded hover:bg-cyan-400 mb-96" onClick={increment}>Increment</button>
+            </div>);
 }
-
-export default App
+export default Counter
